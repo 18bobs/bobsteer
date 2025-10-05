340e4232bdfbc84210787cb6bb7da4a177280545
dinodied <snetchitailo@gmail.com>
2025-10-05 16:13:18 -0700

    add games

--- index.js
+++ index.js
@@ -12,50 +12,15 @@
 
       // mathContainer.style.display = 'none';
       gameHub.style.display = 'flex';
-
-
-  // function generateProblem() {
-  //   const a = Math.floor(Math.random() * 99) + 1;
-  //   const b = Math.floor(Math.random() * 99) + 1;
-  //   currentSum = a + b;
-  //   problemEl.textContent = `${a} + ${b} = ?`;
-  //   feedbackEl.textContent = '';
-  //   answerInput.value = '';
-  //   answerInput.focus();
-  // }
-
-  // generateProblem();
-
-  // answerInput.addEventListener('input', () => {
-  //   const val = answerInput.value.trim();
-
-  //   if (val === 'bre') {
-  //     mathContainer.style.display = 'none';
-  //     gameHub.style.display = 'flex';
-  //     answerInput.value = '';
-  //     return;
-  //   }
-
-  //   if (/^\d+$/.test(val)) {
-  //     if (parseInt(val, 10) === currentSum) {
-  //       feedbackEl.textContent = 'Correct!';
-  //       generateProblem(); // immediately generate next without delay
-  //     } else {
-  //       feedbackEl.textContent = '';
-  //     }
-  //   } else {
-  //     feedbackEl.textContent = '';
-  //   }
-  // });
-
+      
   function openGame(url) {
-    // <iframe is="x-frame-bypass" src="https://example.org/"></iframe>
     iframe.src = url;
     iframe.style.display = 'block';
     backBtn.style.display = 'block';
     gameList.style.display = 'none';
     header.style.display = 'none';
     document.body.style.overflow = 'hidden';
+    
   }
 
   function closeGame() {
